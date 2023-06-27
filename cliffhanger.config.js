f47f9443e6c18910f84e17fa6c4c97f63e91ed97
your friendly trustworthy programmer <117580169+RealSput@users.noreply.github.com>
2023-06-27 16:12:27 -0500

    Update cliffhanger.config.js

--- cliffhanger.config.js
+++ cliffhanger.config.js
@@ -2,13 +2,23 @@ const process = require('./src/process.js');
 const EventEmitter = require('events');
 const fs = require('fs');
 
-let emitter = new class extends EventEmitter {};
+class Emitter extends EventEmitter {};
 
-module.exports = {
+let emitter = new Emitter();
+let options = {};
+
+module.exports = (cwd) => ({
     name: "spwn-plus",
     version: "1.0",
     description: "A superset of SPWN that brings extra features",
     flags: {
+		bundle: {
+			short: "-b",
+			description: "Option to bundle all modules in SPWN+ file",
+			init: () => {
+				options.bundle = { value: true, contents: cwd };
+			}
+		},
         input: {
             short: "-i",
             description: "SPWN+ file to convert to normal SPWN",
@@ -16,8 +26,8 @@ module.exports = {
             amount_of_args: 1,
             init: async (filename) => {
                 let file = fs.readFileSync(filename).toString();
-                let res = await process(file);
-                emitter.emit('output', res);
+                let res = await process(file, options);
+				emitter.emit('output', res);
             },
         },
         output: {
@@ -25,12 +35,12 @@ module.exports = {
             description: "Output file of transpiled SPWN+ file",
             required: true,
             amount_of_args: 1,
-            init: (filename) => {
-                emitter.on('output', (output) => {
-                    console.log('OUT:', output, filename)
-                    fs.writeFileSync(filename, output);
-                });
+            init: (filename) => {	
+				emitter.on('output', (output) => {
+					console.log('OUT:', output, filename)
+					fs.writeFileSync(filename, output);
+				});
             }
         }
     },
-};
+});
